f9f11db56830ba674d5a6dfc747cfd92eb6048ab
Chi Liu <jasoncl666@gmail.com>
2019-12-20 15:59:05 -0800

    hw finished

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var fs = require('fs')
 
 const bodyParser = require('body-parser'); 
 var app = express();
-
 app.use(bodyParser.text({ type: 'text/plain' }));
 app.use(bodyParser.json());
+
+
+const path = process.cwd() + '/'
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -13,15 +16,67 @@ app.get('/', function (req, res) {
 
 
 app.get('/:file', function (req, res) {
-  res.send(req.params);
+
+  var filename = req.params.file;
+
+  fs.readFile(path+filename, function(err, data){
+    if (err) throw err;
+    console.log('content: ' + data.toString());
+  });
 });
 
 
 app.post('/:file', function (req, res) {
-  res.send('Posting file!');
-  var filename = req.body;
-  console.log(filename);
-})
+
+  res.send('New Post created!');
+
+  var filename = req.params.file;  
+  var content = req.body;
+
+  fs.writeFile(path+filename, content, (err)=>{
+    if (err) throw err;
+  });
+});
+
+app.put('/:file', function (req, res) {
+  res.send('Got a PUT request at /user');
+
+  var filename = req.params.file;
+
+  try {
+    if (fs.existsSync(path+filename)) {
+      var content = req.body;
+      const postfix = '.txt';
+
+      var newFilename = filename.split('.')[0]+content+postfix;
+
+      fs.rename(filename, newFilename, (err)=>{
+        if(err) throw err;
+      });      
+    }
+  }catch(err) {
+    console.error(err)
+  }
+});
+
+app.delete('/:file', function (req, res) {
+  res.send('Got a DELETE request at /user')
+
+  var filename = req.params.file;
+  
+  try {
+    if (fs.existsSync(path+filename)) {
+  
+      fs.unlink(path+filename, (err)=>{
+        if(err) throw err;
+      });
+    }
+  }catch(err) {
+    console.error(err)
+  }
+});
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
